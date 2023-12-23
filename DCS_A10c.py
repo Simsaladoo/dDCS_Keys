8d2c545fe82b9a0932486ba2feb33ec8b2c31047
simsa <simsaladoo@hotmail.com>
2023-12-23 14:46:13 -0800

    adding a10c shortcuts

---
+++ DCS_A10c.py
@@ -0,0 +1,24 @@
+# Misc stuff
+LWin + Home # for Startup
+LWin + End # for Shutdown
+LCtrl + E # to Eject
+
+# Takeoff
+LShift + Insert for steering lock
+W wheelbrake
+
+# HOTAS
+H, K # for cycle MFD pages, hold to focus
+M # for Master mode control
+RAlt + Left, Right, Down, Up # for Boat
+LCtrl + Left, Right, Down, Up # for TMS
+C, V # for China Hat Aft/Fwd to zoom
+; , / . # for Slew while MFS focused
+H, J, K, U # for Coolie
+8, 7, 9, 0 # for CMS -- 7 for flares, 8 for Chaff
+
+# Flight keys
+LCtrl + B, LShift + B # for Speed brakes
+LCtrl + F, LShift + F # for Flaps
+LAlt + C # Swap to Mouse Look
+RCtrl + RShift + NumKey # for seat movement
